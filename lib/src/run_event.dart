50939be50936926de49ba82d61a3b7bdf15e394f
Phat <damphat@gmail.com>
2021-03-23 19:29:12 +0700

    nullsafe, equalty

--- lib/src/run_event.dart
+++ lib/src/run_event.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
 class RunEvent {
-  RunEvent({this.kind = '', this.value, this.duration});
+  RunEvent({this.kind = 'event', this.value, this.duration});
 
   String kind;
-  Object value;
-  int duration;
+  Object? value;
+  int? duration;
 
   Color get color {
     final regErr = RegExp(r'err|reject|throw', caseSensitive: false);
-    final regData = RegExp(r'data|resolve', caseSensitive: false);
+    final regData = RegExp(r'return|data|resolve|done', caseSensitive: false);
 
     if (regErr.hasMatch(kind)) return Colors.red;
     if (regData.hasMatch(kind)) return Colors.green;
-    return null;
+    return Colors.black;
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    if (identical(this, other)) return true;
+    if (other is! RunEvent) return false;
+    return (kind == other.kind) &&
+        (duration == other.duration) &&
+        (value == other.value);
+  }
+
+  @override
+  int get hashCode =>
+      kind.hashCode ^ (duration ?? 0).hashCode ^ (value ?? 0).hashCode;
+
+  @override
+  String toString() {
+    return '$kind: ${duration}ms, $value';
   }
 }
--- test/run_event_test.dart
+++ test/run_event_test.dart
@@ -4,8 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('color can be red, green, null', () {
-    expect(RunEvent().color, null);
-    expect(RunEvent(kind: ' xxx ').color, null);
+    expect(RunEvent().color, Colors.black);
+    expect(RunEvent(kind: ' xxx ').color, Colors.black);
 
     expect(RunEvent(kind: ' ERR ').color, Colors.red);
     expect(RunEvent(kind: ' reject ').color, Colors.red);
@@ -13,5 +13,19 @@ void main() {
 
     expect(RunEvent(kind: ' DATA ').color, Colors.green);
     expect(RunEvent(kind: ' resolve ').color, Colors.green);
+  });
+
+  test('equals', () {
+    expect(RunEvent(), RunEvent());
+    expect(RunEvent(kind: 'x'), RunEvent(kind: 'x'));
+    expect(RunEvent(value: 'x'), RunEvent(value: 'x'));
+    expect(RunEvent(duration: 1), RunEvent(duration: 1));
+  });
+
+  test('not equals', () {
+    expect(RunEvent(), RunEvent());
+    expect(RunEvent(kind: 'x'), isNot(RunEvent(kind: 'y')));
+    expect(RunEvent(value: 'x'), isNot(RunEvent(value: 'y')));
+    expect(RunEvent(duration: 0), isNot(RunEvent(value: '1')));
   });
 }
